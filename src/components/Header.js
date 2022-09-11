eae999d81788355115c342c7a5f451fd70a9b62a
Katya Akilina <katyaartline@gmail.com>
2022-09-11 12:22:57 +0300

    feat: add Switch in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,37 @@
 import React from "react";
-import { Link, useHistory, useLocation } from "react-router-dom";
+import { Link, Route, Switch, useHistory, useLocation } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 function Header({ userData, onSignOut }) {
 
-  const location = useLocation();
   const history = useHistory();
-
-  const headertButton = {
-    text: location.pathname === "/signup" ? "Войти" : "Регистрация",
-    link: location.pathname === "/signup" ? "/signin" : "/signup"
-  }
-
   const handlerSubmit = () => {
     onSignOut();
-    history.push("/signin");
+    history.push('signin')
   };
 
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="логотип Место" />
+      <Switch>
+        <Route path="/signup">
+          <Link to="/signin" className="header__mail">Войти</Link>
+        </Route>
+        <Route path="/signin">
+          <Link to="/signup" className="header__mail">Регистрация</Link>
+        </Route>
+        <Route path="/">
+          <button className="header__mail" onClick={handlerSubmit}>Выйти
+          </button>
+        </Route>
+      </Switch>
 
-        <>
+        {/* <>
           <p className="header__mail">{userData.email}</p>
           <Link to={headertButton.link} onClick={handlerSubmit}>
             <p>{headertButton.text}</p>
           </Link>
-        </>
+        </> */}
 
     </header>
   );
